7efb5d68cc5a4581bdc7325d122a43cd266afe56
neutronest <neutronest@gmail.com>
2018-05-12 11:04:45 +0800

    [neutronest] architecture dev (first taste)

---
+++ src/main/scala/com/framex/core/Expr/ExType.scala
@@ -0,0 +1,9 @@
+package com.framex.core.Expr
+
+sealed trait ExType[A]
+
+case object Nan extends ExType[Nothing]
+case class ExInt(i: Int) extends ExType[Int]
+case class ExDouble(d: Double) extends ExType[Double]
+case class ExString(s: String) extends ExType[String]
+
