af72fe3b4771ba7b1cc54753c2a84a57f0de0b51
neutronest <neutronest@gmail.com>
2018-05-14 15:07:13 +0800

    dev

---
+++ src/main/scala/com/framex/core/SeqX.scala
@@ -0,0 +1,15 @@
+package com.framex.core
+import com.framex.core.Expr.Expr
+
+class SeqX[A] extends BaseFrame[A] {
+
+  def SeqX(): Unit = {
+
+  }
+
+  override def data: Expr[A] = ???
+
+  override def ndim: Int = ???
+
+  override def shape: List[Int] = ???
+}
