1bc039b279709c3ea61facfc78dfa0ae73199770
neutronest <neutronest@gmail.com>
2018-05-16 10:26:36 +0800

    [neutronest] refactor FrameX

--- src/main/scala/com/framex/core/FrameX.scala
+++ src/main/scala/com/framex/core/FrameX.scala
@@ -1,43 +1,30 @@
 package com.framex.core
 
+import com.framex.core.Expr.BottomType
+
 import scala.reflect.ClassTag
 
 
-class FrameX() {
-
-  val data: Vector[Vector[ElemX[_]]] = ???
+class FrameX()(var data: Vector[Vector[ElemX]]) {
 }
 
 object FrameX {
-
-  def apply(data_ : Vector[Vector[ElemX[_]]]): FrameX = {
+  def apply(data_ : Vector[Vector[ElemX]]): FrameX = {
     FrameX(data_)
   }
 
-  def applyOne[A](data_ : Vector[ElemX[A]]): FrameX = {
+  def applyOne[A](data_ : Vector[ElemX]): FrameX = {
 
     var frame = Vector()
     FrameX(frame :+ data_)
   }
 
-  def fromList(ll: List[List[_]])(implicit elemCT: ClassTag[ElemX[_]]): FrameX = {
-
-
-    var baz : List[Vector[ElemX[_]]] = List()
-    for (l <- ll) {
-      var bb : List[ElemX[_]] = List()
-      for (item <- l) {
-        bb = bb :+ ElemX(item)
-      }
-      baz = baz :+ bb.toVector
-    }
-    apply(baz.toVector)
-
-//    var foobar : Vector[Vector[ElemX[_]]] = ll.map(
-//      (l: List[Any]) => l.map(
-//      item => new ElemX[_](ElemX.from(item))).toVector)
-//      .toVector
-//    apply(foobar)
+  def fromList[A: BottomType](ll: List[List[A]])(implicit elemCT: ClassTag[ElemX]): FrameX = {
+    var foobar : Vector[Vector[ElemX]] = ll.map(
+      l => l.map(
+      item => ElemX(item)).toVector)
+      .toVector
+    apply(foobar)
   }
 
 
