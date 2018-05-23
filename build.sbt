26f7dafc406d62730661123880f1d9a1fb53017f
neutronest <neutronest@gmail.com>
2018-05-23 18:46:18 +0800

    @neutronest column name support dev

--- build.sbt
+++ build.sbt
@@ -1,14 +1,16 @@
 import Dependencies._
+
 lazy val root = (project in file(".")).settings(
   inThisBuild(List(
     organization := "com.framex",
     scalaVersion := "2.12.3",
-    version      := "0.1.0-SNAPSHOT"
+    version := "0.1.0-SNAPSHOT"
   )),
   name := "framex",
   libraryDependencies += scalaTest % Test,
   libraryDependencies += "org.scala-lang" % "scala-reflect" % "2.12.6",
-  libraryDependencies += "com.github.tototoshi" %% "scala-csv" % "1.3.5"
+  libraryDependencies += "com.github.tototoshi" %% "scala-csv" % "1.3.5",
+  libraryDependencies += "org.scalaz" %% "scalaz-core" % "7.3.0-M23"
 
 
 )
--- src/test/scala/com/framex/TestFrameX.scala
+++ src/test/scala/com/framex/TestFrameX.scala
@@ -35,6 +35,16 @@ class TestFrameX extends FlatSpec with Matchers {
     df.shape().equals((5, 3)) shouldEqual true
   }
 
+  it should "test column name" in {
+    val ll = List(List(1, 2, 3, 4, 5),
+      List("A", "B", "C", "D", "E"),
+      List("2015-01-10", "2017-08-22", "2016-03-03", "2011-02-02", "2017-02-12"))
+    val columnNames = List("id", "word", "date")
+    val df = FrameX.fromList(ll, columnNames)
+    df("date").data.foreach(f => f.foreach(f2 => println(f2.elem)))
+    df("date").equals(FrameX.fromList(List(List("2015-01-10", "2017-08-22", "2016-03-03", "2011-02-02", "2017-02-12")))) shouldEqual(true)
+  }
+
 //  "Performance test" should "cost small time" in {
 //
 //    var ll : List[List[Int]] = (for {
