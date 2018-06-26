7f7bb570259a8b7f3ef846bb5b12bb0ee1326ad0
neutronest <neutronest@gmail.com>
2018-06-26 14:55:14 +0800

    @neutronest: Dev: add ND4J dependency

---
+++ src/main/scala/com/framex/io/CsvHandler.scala
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2018.
+ */
+
+package com.framex.io
+
+import com.framex.core.FrameX
+
+object CsvHandler {
+
+  def readCSV(fileName: String,
+              sep: String,
+              header: String
+             ) : FrameX = {
+
+    ???
+  }
+
+  def toCSV(df: FrameX, fileName: String) : Unit = {
+    ???
+  }
+
+  def readND4J(data: Any) : FrameX = {
+    ???
+  }
+
+  def toND4J(df: FrameX) : Any = {
+    ???
+  }
+
+
+}
