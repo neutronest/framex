7160c6878264d42a78b0080885192468d2a04604
neutronest <neutronest@gmail.com>
2018-09-08 00:21:35 +0800

    tset

--- src/test/scala/com/framex/TestTField.scala
+++ src/test/scala/com/framex/TestTField.scala
@@ -4,19 +4,26 @@
 
 package com.framex
 
+import com.framex.core.{IntField, TField}
+import com.framex.core.TField.CoyoTField
 import org.scalatest.{FlatSpec, Matchers}
-
+import scalaz.Coyoneda
 
 class TestTField extends FlatSpec with Matchers {
 
 
   it should "test coyoneda" in {
 
-//    val n = implicitly[CoyoTField[Int]]
-//    import n._
-//
-//    val fn = (x :Int) => x + 2
-//    val t1 : IntField = IntField(2)
+    import scalaz._
+
+    val fn = (x :Int) => x + 2
+    val t1 : Coyoneda[TField, Int] = Coyoneda.lift(IntField(2))
+    print(t1.fi)
+    val t2 = t1.map(fn)
+    print(t2.fi)
+    print("foobar")
+
+
 //
 //    val ct2 = TField.fmap(n)(fn)
 //    print("axibabababab")
